d8f0c2e4ec7cbad1bc15974a95095db2abddbd89
aloctavodia <aloctavodia@gmail.com>
2023-10-26 12:35:56 -0300

    fix test

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -2,18 +2,9 @@
 """Test batteries-included plots."""
 import numpy as np
 import pytest
-from arviz_base import convert_to_datatree
+from arviz_base import from_dict
 
 from arviz_plots import plot_dist, plot_trace
-
-
-@pytest.fixture(scope="module")
-def data(seed=31):
-    rng = np.random.default_rng(seed)
-    mu = rng.normal(size=(4, 100))
-    theta = rng.normal(size=(4, 100, 7))
-
-    return convert_to_datatree({"mu": mu, "theta": theta})
 
 
 @pytest.fixture(scope="module")
@@ -22,19 +13,19 @@ def datatree(seed=31):
     mu = rng.normal(size=(4, 100))
     theta = rng.normal(size=(4, 100, 7))
 
-    return convert_to_datatree({"mu": mu, "theta": theta})
+    return from_dict({"posterior": {"mu": mu, "theta": theta}}, dims={"theta": ["hierarchy"]})
 
 
 @pytest.mark.parametrize("backend", ["matplotlib", "bokeh"])
 class TestPlots:
-    def test_plot_dist(self, data, backend):
-        pc = plot_dist(data, backend=backend)
+    def test_plot_dist(self, datatree, backend):
+        pc = plot_dist(datatree, backend=backend)
         assert not pc.aes["mu"]
         assert "kde" in pc.viz["mu"]
-        assert "theta_dim_0" not in pc.viz["mu"].dims
-        assert "theta_dim_0" in pc.viz["theta"].dims
-        assert "theta_dim_0" not in pc.viz["mu"]["point_estimate"].dims
-        assert "theta_dim_0" in pc.viz["theta"]["point_estimate"].dims
+        assert "hierarchy" not in pc.viz["mu"].dims
+        assert "hierarchy" in pc.viz["theta"].dims
+        assert "hierarchy" not in pc.viz["mu"]["point_estimate"].dims
+        assert "hierarchy" in pc.viz["theta"]["point_estimate"].dims
 
     def test_plot_trace(self, datatree, backend):
         pc = plot_trace(datatree, var_names=["mu"], backend=backend)
