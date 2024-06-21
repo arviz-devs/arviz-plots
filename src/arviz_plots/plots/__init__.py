bbbd441e23e20d5bb46c7ff02861ec01aedde730
Ratish Panda <ratishp2001@gmail.com>
2024-06-21 15:37:02 +0530

    added ppcplot-beta

--- src/arviz_plots/plots/__init__.py
+++ src/arviz_plots/plots/__init__.py
@@ -3,9 +3,10 @@
 from .compareplot import plot_compare
 from .distplot import plot_dist
 from .forestplot import plot_forest
-from .ridgeplot import plot_ridge
+from .ppcplot import plot_ppc
 from .tracedistplot import plot_trace_dist
 from .traceplot import plot_trace
+from .ridgeplot import plot_ridge
 
 __all__ = [
     "plot_compare",
@@ -14,4 +15,5 @@ __all__ = [
     "plot_trace",
     "plot_trace_dist",
     "plot_ridge",
+    "plot_ppc",
 ]
