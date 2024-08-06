57a3646c73f19ed79def2b0fb3ec7bf8adb88ae4
Oriol Abril-Pla <oriol.abril.pla@gmail.com>
2024-08-06 17:54:10 +0200

    Apply suggestions from code review
    Co-authored-by: Osvaldo A Martin <aloctavodia@gmail.com>

--- src/arviz_plots/backend/__init__.py
+++ src/arviz_plots/backend/__init__.py
@@ -3,8 +3,8 @@
 Each submodule within this module defines a common interface layer to different plotting libraries.
 
 Outside ``arviz_plots.backend`` the corresponding backend module is imported,
-but only the common interface layer is used, maing no distinctions between plotting backends.
-Each submodule inside ``arviz_plots.backend`` is expected to implement the same funcions
+but only the common interface layer is used, making no distinctions between plotting backends.
+Each submodule inside ``arviz_plots.backend`` is expected to implement the same functions
 with the same call signature. Thus, adding a new backend requires only
 implementing this common interface for it, with no changes to any of the other modules.
 """
