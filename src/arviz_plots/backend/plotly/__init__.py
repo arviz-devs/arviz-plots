b6c03274b09394e37fe5f503fa3aa8b645712437
Oriol Abril-Pla <oriol.abril.pla@gmail.com>
2024-08-08 03:19:04 +0200

    fix 1x1 grid generation in plotly

--- src/arviz_plots/backend/plotly/__init__.py
+++ src/arviz_plots/backend/plotly/__init__.py
@@ -263,8 +263,6 @@ def create_plotting_grid(
     for row in range(rows):
         for col in range(cols):
             plots[row, col] = PlotlyPlot(chart, row + 1, col + 1)
-    if squeeze and plots.size == 1:
-        return None, plots[0, 0]
     return chart, plots.squeeze() if squeeze else plots
 
 
