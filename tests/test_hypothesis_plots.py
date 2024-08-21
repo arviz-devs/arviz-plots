20ff025baf68c1874f330e87c96b0bf0fd677d52
Ratish Panda <ratishp2001@gmail.com>
2024-08-21 15:35:40 +0530

    tests for essplot

--- tests/test_hypothesis_plots.py
+++ tests/test_hypothesis_plots.py
@@ -8,7 +8,7 @@ from arviz_base import from_dict
 from datatree import DataTree
 from hypothesis import given
 
-from arviz_plots import plot_dist, plot_forest, plot_ridge
+from arviz_plots import plot_dist, plot_ess, plot_forest, plot_ridge
 
 pytestmark = pytest.mark.usefixtures("no_artist_kwargs")
 
@@ -191,4 +191,64 @@ def test_plot_ridge(datatree, combined, plot_kwargs, labels_shade_label):
             else:
                 assert all(key in child for child in pc.viz.children.values())
         elif key not in ("remove_axis", "ticklabels"):
+            assert all(key in child for child in pc.viz.children.values())
+
+
+ess_kind_value = st.sampled_from(("local", "quantile"))
+ess_relative = st.booleans()
+ess_rug = st.booleans()
+ess_extra_methods = st.booleans()
+
+
+@st.composite
+def ess_n_points(draw):
+    return draw(st.integers(min_value=1, max_value=50))  # should this range be changed?
+
+
+@st.composite
+def ess_min_ess(draw):
+    return draw(st.integers(min_value=10, max_value=150))  # max samples = 3 x 50 = 150
+
+
+@given(
+    plot_kwargs=st.fixed_dictionaries(
+        {},
+        optional={
+            "ess": plot_kwargs_value,
+            "rug": st.sampled_from(({}, {"color": "red"})),
+            "xlabel": st.sampled_from(({}, {"color": "red"})),
+            "ylabel": st.sampled_from(({}, {"color": "red"})),
+            "mean": plot_kwargs_value,
+            "mean_text": plot_kwargs_value,
+            "sd": plot_kwargs_value,
+            "sd_text": plot_kwargs_value,
+            "min_ess": plot_kwargs_value,
+            "title": plot_kwargs_value,
+            "remove_axis": st.just(False),
+        },
+    ),
+    kind=ess_kind_value,
+    relative=ess_relative,
+    rug=ess_rug,
+    n_points=ess_n_points(),
+    extra_methods=ess_extra_methods,
+    min_ess=ess_min_ess(),
+)
+def test_plot_ess(datatree, kind, relative, rug, n_points, extra_methods, min_ess, plot_kwargs):
+    pc = plot_ess(
+        datatree,
+        backend="none",
+        kind=kind,
+        relative=relative,
+        rug=rug,
+        n_points=n_points,
+        extra_methods=extra_methods,
+        min_ess=min_ess,
+        plot_kwargs=plot_kwargs,
+    )
+    assert all("plot" in child for child in pc.viz.children.values())
+    for key, value in plot_kwargs.items():
+        if value is False:
+            assert all(key not in child for child in pc.viz.children.values())
+        elif key != "remove_axis":
             assert all(key in child for child in pc.viz.children.values())
