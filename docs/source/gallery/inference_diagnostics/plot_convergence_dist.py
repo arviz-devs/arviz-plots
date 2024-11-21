a23d3fcdcf3f085a4992529bfc7fd9f2fab172f9
aloctavodia <aloctavodia@gmail.com>
2024-11-21 14:48:38 -0300

    rename, add example

---
+++ docs/source/gallery/inference_diagnostics/plot_convergence_dist.py
@@ -0,0 +1,25 @@
+"""
+# Convergence diagnostics distribution plot
+
+Plot the distribution of ESS and R-hat.
+
+---
+
+:::{seealso}
+API Documentation: {func}`~arviz_plots.plot_ess`
+:::
+"""
+
+from arviz_base import load_arviz_data
+
+import arviz_plots as azp
+
+azp.style.use("arviz-clean")
+
+data = load_arviz_data("radon")
+pc = azp.plot_ess(
+    data,
+    var_names=["za_county"],
+    backend="none",  # change to preferred backend
+)
+pc.show()
