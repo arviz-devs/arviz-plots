b4baa4cd710c65b6c4fd1a252d93e601bbfb1ae8
aloctavodia <aloctavodia@gmail.com>
2025-02-05 14:47:19 +0200

    add cetrino and vibrant styles to plotly

--- src/arviz_plots/backend/plotly/templates.py
+++ src/arviz_plots/backend/plotly/templates.py
@@ -23,3 +23,51 @@ arviz_variat_template.layout.colorway = [
     "#6f6f6f",
     "#b7b7b7",
 ]
+
+
+arviz_cetrino_template = go.layout.Template()
+
+arviz_cetrino_template.layout.paper_bgcolor = "white"
+arviz_cetrino_template.layout.plot_bgcolor = "white"
+arviz_cetrino_template.layout.polar.bgcolor = "white"
+arviz_cetrino_template.layout.ternary.bgcolor = "white"
+axis_common = {"showgrid": False, "ticks": "outside", "showline": True, "zeroline": False}
+arviz_cetrino_template.layout.xaxis = axis_common
+arviz_cetrino_template.layout.yaxis = axis_common
+
+arviz_cetrino_template.layout.colorway = [
+    "#009988",
+    "#9238b2",
+    "#d2225f",
+    "#ec8f26",
+    "#fcd026",
+    "#3cd186",
+    "#a57119",
+    "#2f5e14",
+    "#f225f4",
+    "#8f9fbf",
+]
+
+
+arviz_vibrant_template = go.layout.Template()
+
+arviz_vibrant_template.layout.paper_bgcolor = "white"
+arviz_vibrant_template.layout.plot_bgcolor = "white"
+arviz_vibrant_template.layout.polar.bgcolor = "white"
+arviz_vibrant_template.layout.ternary.bgcolor = "white"
+axis_common = {"showgrid": False, "ticks": "outside", "showline": True, "zeroline": False}
+arviz_vibrant_template.layout.xaxis = axis_common
+arviz_vibrant_template.layout.yaxis = axis_common
+
+arviz_vibrant_template.layout.colorway = [
+    "#008b92",
+    "#f15c58",
+    "#48cdef",
+    "#98d81a",
+    "#997ee5",
+    "#f5dc9d",
+    "#c90a4e",
+    "#145393",
+    "#323232",
+    "#616161",
+]
