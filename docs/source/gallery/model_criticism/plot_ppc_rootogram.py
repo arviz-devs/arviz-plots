863d2d6096cb21e76bbba4b914964f6002d9257a
aloctavodia <aloctavodia@gmail.com>
2025-02-21 15:44:53 +0200

    fix examples

--- docs/source/gallery/model_criticism/plot_ppc_rootogram.py
+++ docs/source/gallery/model_criticism/plot_ppc_rootogram.py
@@ -18,6 +18,8 @@ azp.style.use("arviz-variat")
 dt = load_arviz_data("rugby")
 pc = azp.plot_ppc_rootogram(
     dt,
+    pc_kwargs={"aes": {"color": ["__variable__"]}}, # map variable to color
+    aes_map={"title": ["color"]}, # change title's color per variable
     backend="none",
 )
 pc.show()
