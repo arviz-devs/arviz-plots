8406363befb0c73d37e8680bbf714434bdc81bc7
aloctavodia <aloctavodia@gmail.com>
2025-02-25 11:56:17 +0200

    consistence data_pairs, remove markers pava

--- src/arviz_plots/plots/ppcdistplot.py
+++ src/arviz_plots/plots/ppcdistplot.py
@@ -15,6 +15,7 @@ from arviz_plots.visuals import ecdf_line, hist, line_xy
 
 def plot_ppc_dist(
     dt,
+    data_pairs=None,
     var_names=None,
     filter_vars=None,
     group="posterior_predictive",
@@ -37,6 +38,10 @@ def plot_ppc_dist(
     ----------
     dt : DataTree
         Input data
+    data_pairs : dict, optional
+        Dictionary of keys prior/posterior predictive data and values observed data variable names.
+        If None, it will assume that the observed data and the predictive data have
+        the same variable name.
     var_names : str or list of str, optional
         One or more variables to be plotted.
         Prefix the variables by ~ when you want to exclude them from the plot.
@@ -155,12 +160,17 @@ def plot_ppc_dist(
         dims for dims in dt.posterior_predictive.dims if dims not in sample_dims
     ]
 
+    if data_pairs is None:
+        data_pairs = (var_names, var_names)
+    else:
+        data_pairs = (list(data_pairs.keys()), list(data_pairs.values()))
+
     predictive_dist = process_group_variables_coords(
-        dt, group=group, var_names=var_names, filter_vars=filter_vars, coords=coords
+        dt, group=group, var_names=data_pairs[0], filter_vars=filter_vars, coords=coords
     )
 
     observed_dist = process_group_variables_coords(
-        dt, group="observed_data", var_names=var_names, filter_vars=filter_vars, coords=coords
+        dt, group="observed_data", var_names=data_pairs[1], filter_vars=filter_vars, coords=coords
     )
 
     predictive_types = [
