d24239c489a7dfe126bb7d047707d8feb6f02dfd
aloctavodia <aloctavodia@gmail.com>
2025-03-04 14:11:20 +0200

    clarify text example

--- docs/source/gallery/posterior_predictive_checks/plot_ppc_pit.py
+++ docs/source/gallery/posterior_predictive_checks/plot_ppc_pit.py
@@ -4,7 +4,8 @@
 Plot of $p(\tilde y_i \le y_i \mid y)$, where $y_i$ represents the observed data for index $i$
 and $\tilde y_i$ represents the posterior predictive data for index $i$.
 
-The distribution should be uniform if the model is well-calibrated.
+The distribution should be uniform if the model is well-calibrated. As small deviations from
+uniformity are expected, the plot also shows the credible bands. 
 ---
 
 :::{seealso}
