34e41fd5f0579e1342b87a5bddc706f19bc927cf
aloctavodia <aloctavodia@gmail.com>
2025-03-07 16:07:58 +0200

    use col_wrap in some of the examples

--- docs/source/gallery/distribution/plot_dist_ecdf.py
+++ docs/source/gallery/distribution/plot_dist_ecdf.py
@@ -19,6 +19,7 @@ data = load_arviz_data("centered_eight")
 pc = azp.plot_dist(
     data,
     kind="ecdf",
+    pc_kwargs={"col_wrap": 4},
     backend="none"  # change to preferred backend
 )
 pc.show()
