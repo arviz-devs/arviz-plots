e305ee6f950b964fe36af2d06779b90f1b567d96
aloctavodia <aloctavodia@gmail.com>
2025-04-22 08:20:17 +0300

    add title custom function, fix docstring, remove cv

--- src/arviz_plots/plots/ppc_tstat.py
+++ src/arviz_plots/plots/ppc_tstat.py
@@ -55,9 +55,10 @@ def plot_ppc_tstat(
         Defaults to ``rcParams["data.sample_dims"]``
     t_stat : str, float, or callable() default "median"
         Test statistics to compute from the observations and predictive distributions.
-        Allowed strings are “mean”, “median” or “std”. Alternative a quantile can be passed
-        as a float (or str) in the interval (0, 1). Finally, a user defined function is also
-        accepted.
+        Allowed strings are “mean”, “median”, “std”, “var”, “min”, “max”, “iqr”
+        (interquartile range) and “mad” (mean absolute deviation). Alternative a
+        quantile can be passed as a float (or str) in the interval (0, 1). Finally,
+        a user defined function is also accepted.
     kind : {"kde", "hist", "dot", "ecdf"}, optional
         How to represent the marginal density.
         Defaults to ``rcParams["plot.density_kind"]``
@@ -130,7 +131,7 @@ def plot_ppc_tstat(
 
         >>> def cv(x):
         >>>     return np.std(x, axis=0) / np.mean(x, axis=0)
-        >>> plot_ppc_tstat(dt, t_stat=lambda x: cv(x), kind="hist")
+        >>> plot_ppc_tstat(dt, t_stat=cv, kind="hist")
 
 
     Use median as t-statistic and plot point-interval
@@ -223,20 +224,10 @@ def plot_ppc_tstat(
         observed_dist = mad(observed_dist, dim=None)
         plot_kwargs.setdefault("title", {"text": "MAD"})
 
-    elif t_stat == "cv":
-
-        def cv(data, dim):
-            mean = data.mean(dim=dim)
-            std = data.std(dim=dim)
-            return std / mean
-
-        predictive_dist = cv(predictive_dist, dim=reduce_dim)
-        observed_dist = cv(observed_dist, dim=None)
-        plot_kwargs.setdefault("title", {"text": "CV"})
-
     elif hasattr(t_stat, "__call__"):
         predictive_dist = predictive_dist.map(t_stat)
         observed_dist = observed_dist.map(t_stat)
+        plot_kwargs.setdefault("title", {"text": t_stat.__name__})
     else:
         try:
             t_stat_float = float(t_stat)
