7fb9d9c775155d403f93e9beeb5e0d438028411e
aloctavodia <aloctavodia@gmail.com>
2025-05-13 13:09:12 +0300

    update docs

---
+++ docs/source/gallery/mixed/00_plot_rank_dist.py
@@ -0,0 +1,23 @@
+"""
+# Rank and distribution plot
+
+Two column layout with marginal distributions on the left and fractional ranks on the right
+
+---
+
+:::{seealso}
+API Documentation: {func}`~arviz_plots.plot_rank_dist`
+:::
+"""
+from arviz_base import load_arviz_data
+
+import arviz_plots as azp
+
+azp.style.use("arviz-variat")
+
+data = load_arviz_data("non_centered_eight")
+pc = azp.plot_rank_dist(
+    data,
+    backend="none"  # change to preferred backend
+)
+pc.show()
