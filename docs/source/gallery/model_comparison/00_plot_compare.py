143f2b37e941bb7469bb183569fa3d9fd9a53173
The-Broken-Keyboard <saurabhshreyansh868@gmail.com>
2025-05-26 00:14:50 +0530

    fixed column name in gallery generator code

--- docs/source/gallery/model_comparison/00_plot_compare.py
+++ docs/source/gallery/model_comparison/00_plot_compare.py
@@ -17,7 +17,7 @@ import arviz_plots as azp
 azp.style.use("arviz-variat")
 
 
-cmp_df = pd.DataFrame({"elpd": [-4.5, -14.3, -16.2], 
+cmp_df = pd.DataFrame({"elpd_loo": [-4.5, -14.3, -16.2], 
                        "p": [2.6, 2.3, 2.1], 
                        "elpd_diff": [0, 9.7, 11.3], 
                        "weight": [0.9, 0.1, 0], 
