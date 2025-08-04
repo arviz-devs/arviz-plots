bbdf815898c75efe490533026a6169a695ac8e0d
aloctavodia <aloctavodia@gmail.com>
2025-08-04 13:18:01 +0300

    update plotly template

--- src/arviz_plots/backend/plotly/templates.py
+++ src/arviz_plots/backend/plotly/templates.py
@@ -1,17 +1,55 @@
 """Plotly templates for ArviZ styles."""
 import plotly.graph_objects as go
 
-arviz_variat_template = go.layout.Template()
+axis_common = {"showgrid": False, "ticks": "outside", "showline": True, "zeroline": False}
 
+arviz_cetrino_template = go.layout.Template()
+arviz_cetrino_template.layout.paper_bgcolor = "white"
+arviz_cetrino_template.layout.plot_bgcolor = "white"
+arviz_cetrino_template.layout.polar.bgcolor = "white"
+arviz_cetrino_template.layout.ternary.bgcolor = "white"
+arviz_cetrino_template.layout.margin = {"l": 50, "r": 10, "t": 40, "b": 45}
+arviz_cetrino_template.layout.xaxis = axis_common
+arviz_cetrino_template.layout.yaxis = axis_common
+arviz_cetrino_template.layout.colorway = [
+    "#3cd186",
+    "#fcd026",
+    "#ec8f26",
+    "#c969eb",
+    "#2b8c92",
+    "#a91a32",
+    "#2f2cec",
+    "#958152",
+]
+
+arviz_tenui_template = go.layout.Template()
+arviz_tenui_template.layout.paper_bgcolor = "white"
+arviz_tenui_template.layout.plot_bgcolor = "white"
+arviz_tenui_template.layout.polar.bgcolor = "white"
+arviz_tenui_template.layout.ternary.bgcolor = "white"
+arviz_tenui_template.layout.margin = {"l": 50, "r": 10, "t": 40, "b": 45}
+arviz_tenui_template.layout.xaxis = axis_common
+arviz_tenui_template.layout.yaxis = axis_common
+arviz_tenui_template.layout.colorway = [
+    "#84d3ee",
+    "#e7b08b",
+    "#f7ea8d",
+    "#b6e4d0",
+    "#c88bb8",
+    "#5bb2ed",
+    "#7b9091",
+    "#c7b4c0",
+]
+
+
+arviz_variat_template = go.layout.Template()
 arviz_variat_template.layout.paper_bgcolor = "white"
 arviz_variat_template.layout.plot_bgcolor = "white"
 arviz_variat_template.layout.polar.bgcolor = "white"
 arviz_variat_template.layout.ternary.bgcolor = "white"
 arviz_variat_template.layout.margin = {"l": 50, "r": 10, "t": 40, "b": 45}
-axis_common = {"showgrid": False, "ticks": "outside", "showline": True, "zeroline": False}
 arviz_variat_template.layout.xaxis = axis_common
 arviz_variat_template.layout.yaxis = axis_common
-
 arviz_variat_template.layout.colorway = [
     "#36acc6",
     "#f66d7f",
@@ -20,57 +58,25 @@ arviz_variat_template.layout.colorway = [
     "#228306",
     "#a252f4",
     "#63f0ea",
-    "#000000",
-    "#6f6f6f",
-    "#b7b7b7",
-]
-
-
-arviz_cetrino_template = go.layout.Template()
-
-arviz_cetrino_template.layout.paper_bgcolor = "white"
-arviz_cetrino_template.layout.plot_bgcolor = "white"
-arviz_cetrino_template.layout.polar.bgcolor = "white"
-arviz_cetrino_template.layout.ternary.bgcolor = "white"
-arviz_cetrino_template.layout.margin = {"l": 50, "r": 10, "t": 40, "b": 45}
-axis_common = {"showgrid": False, "ticks": "outside", "showline": True, "zeroline": False}
-arviz_cetrino_template.layout.xaxis = axis_common
-arviz_cetrino_template.layout.yaxis = axis_common
-
-arviz_cetrino_template.layout.colorway = [
-    "#009988",
-    "#9238b2",
-    "#d2225f",
-    "#ec8f26",
-    "#fcd026",
-    "#3cd186",
-    "#a57119",
-    "#2f5e14",
-    "#f225f4",
-    "#8f9fbf",
+    "#a77e4f",
 ]
 
 
 arviz_vibrant_template = go.layout.Template()
-
 arviz_vibrant_template.layout.paper_bgcolor = "white"
 arviz_vibrant_template.layout.plot_bgcolor = "white"
 arviz_vibrant_template.layout.polar.bgcolor = "white"
 arviz_vibrant_template.layout.ternary.bgcolor = "white"
 arviz_vibrant_template.layout.margin = {"l": 50, "r": 10, "t": 40, "b": 45}
-axis_common = {"showgrid": False, "ticks": "outside", "showline": True, "zeroline": False}
 arviz_vibrant_template.layout.xaxis = axis_common
 arviz_vibrant_template.layout.yaxis = axis_common
-
 arviz_vibrant_template.layout.colorway = [
     "#008b92",
     "#f15c58",
     "#48cdef",
-    "#98d81a",
-    "#997ee5",
-    "#f5dc9d",
+    "#bc56f0",
+    "#8ddb17",
+    "#f9cf9c",
     "#c90a4e",
-    "#145393",
-    "#323232",
-    "#616161",
+    "#b0aaa2",
 ]
