6323ac1b4f5d463b6edb152d2668c665d271352a
aloctavodia <aloctavodia@gmail.com>
2025-09-10 09:22:52 +0300

    draft alt plot_lm

--- src/arviz_plots/plots/lm_plot.py
+++ src/arviz_plots/plots/lm_plot.py
@@ -1,7 +1,7 @@
 """lm plot code."""
 
 import warnings
-from collections.abc import Mapping, Sequence
+from collections.abc import Mapping
 from copy import copy
 from importlib import import_module
 from typing import Any, Literal
@@ -9,14 +9,13 @@ from typing import Any, Literal
 import arviz_stats as azs
 import numpy as np
 import xarray as xr
-from arviz_base import rcParams
+from arviz_base import extract, rcParams
 from arviz_base.labels import BaseLabeller
 
 from arviz_plots.plot_collection import PlotCollection
 from arviz_plots.plots.utils import (
     filter_aes,
     get_contrast_colors,
-    get_group,
     process_group_variables_coords,
     set_wrap_layout,
 )
@@ -35,7 +34,7 @@ def plot_lm(
     sample_dims=None,
     ci_kind=None,
     ci_prob=None,
-    line_kind=None,
+    point_estimate=None,
     plot_collection=None,
     backend=None,
     labeller=None,
@@ -43,18 +42,18 @@ def plot_lm(
         Literal[
             "ci_line",
             "central_line",
-            "ci_fill",
+            "ci_band",
             "scatter",
             "xlabel",
             "ylabel",
         ],
-        Sequence[str],
+        list[str],
     ] = None,
     visuals: Mapping[
         Literal[
             "ci_line",
             "central_line",
-            "ci_fill",
+            "ci_band",
             "scatter",
             "xlabel",
             "ylabel",
@@ -77,15 +76,6 @@ def plot_lm(
         Target variable. If None (default), the first variable in "observed_data" is used.
     x : str or list of str or DataArray or Dataset, optional
         Independent variable(s). If None (default), all variables in "constant_data" are used.
-    y_pred : str or DataArray, optional
-        Predicted values.
-        If None (default), the variable in the specified group with the same name as y is used.
-    x_pred : str or list of str or DataArray or Dataset, optional
-        Independent variable(s) for predictions.
-
-        If None (default), and if group is "predictions", all variables corresponding to x data
-        in "predictions_constant_data" group are used. If group is "posterior_predictive",
-        x is used.
     filter_vars: {None, “like”, “regex”}, default None
         If None (default), interpret var_names as the real variables names.
         If “like”, interpret var_names as substrings of the real variables names.
@@ -100,11 +90,11 @@ def plot_lm(
         Defaults to ``rcParams["data.sample_dims"]``
     ci_kind : {"hdi", "eti"}, optional
         Which credible interval to use. Defaults to ``rcParams["stats.ci_kind"]``
-    ci_prob : float or list of float, optional
+    ci_prob : float or array-like of floats, optional
         Indicates the probabilities that should be contained within the plotted credible intervals.
         Defaults to ``rcParams["stats.ci_prob"]``
-    line_kind : {"mean", "median","mode"}, optional
-        Which point estimate to use for the line. Defaults to ``rcParams["stats.point_estimate"]``
+    point_estimate : {"mean", "median","mode"}, optional
+        Which point_estimate to use for the line. Defaults to ``rcParams["stats.point_estimate"]``
     plot_collection : PlotCollection, optional
     backend : {"matplotlib", "bokeh"}, optional
     labeller : labeller, optional
@@ -116,9 +106,9 @@ def plot_lm(
         Valid keys are:
 
         * ci_line -> passed to :func:`~.visuals.line_xy`. Defaults to False
-        * central_line -> passed to :func:`~.visuals.line_xy`.
-        * ci_fill -> passed to :func:`~.visuals.fill_between_y`.
-        * scatter -> passed to :func:`~.visuals.scatter_xy`.
+        * pe_line-> passed to :func:`~.visuals.line_xy`.
+        * ci_band -> passed to :func:`~.visuals.fill_between_y`.
+        * observed_scatter -> passed to :func:`~.visuals.scatter_xy`.
         * xlabel -> passed to :func:`~.visuals.labelled_x`.
         * ylabel -> passed to :func:`~.visuals.labelled_y`.
 
@@ -165,76 +155,109 @@ def plot_lm(
 
     if labeller is None:
         labeller = BaseLabeller()
-    if line_kind is None:
-        line_kind = rcParams["stats.point_estimate"]
+    if point_estimate is None:
+        point_estimate = rcParams["stats.point_estimate"]
     if backend is None:
         if plot_collection is None:
             backend = rcParams["plot.backend"]
         else:
             backend = plot_collection.backend
 
-    obs_data = get_group(dt, "observed_data")
-    if y is None:
-        y = list(obs_data.data_vars)[0]
+    if point_estimate not in ("mean", "median", "mode"):
+        raise ValueError("point_estimate must be one of 'mean', 'median', or 'mode'")
 
-    if isinstance(y, xr.Dataset):
-        raise TypeError(
-            "y can't be a dataset because multiple target variables are not supported yet."
-        )
+    # obs_data = get_group(dt, "observed_data")
+    # if y is None:
+    #     y = list(obs_data.data_vars)[0]
 
-    if not isinstance(y, xr.DataArray):
-        y = process_group_variables_coords(
-            dt, group="observed_data", var_names=y, filter_vars=filter_vars, coords=coords
-        )
+    # if isinstance(y, xr.Dataset):
+    #     raise TypeError(
+    #         "y can't be a dataset because multiple target variables are not supported yet."
+    #     )
 
-    const_data = get_group(dt, "constant_data")
-    if x is None:
-        x = list(const_data.data_vars)
+    # if not isinstance(y, xr.DataArray):
+    #     y = process_group_variables_coords(
+    #         #dt, group="posterior", var_names=y, filter_vars=filter_vars, coords=coords
+    #         dt, group="observed_data", var_names=y, filter_vars=filter_vars, coords=coords
+    #     )
 
-    if not isinstance(x, xr.DataArray | xr.Dataset):
-        x = process_group_variables_coords(
-            dt, group="constant_data", var_names=x, filter_vars=filter_vars, coords=coords
-        )
+    # const_data = get_group(dt, "constant_data")
+    # if x is None:
+    #     x = list(const_data.data_vars)
 
-    (target_var,) = y.data_vars
-    independent_var = list(x.data_vars)
+    # if not isinstance(x, xr.DataArray | xr.Dataset):
+    #     x = process_group_variables_coords(
+    #         dt, group="constant_data", var_names=x, filter_vars=filter_vars, coords=coords
+    #     )
 
-    if y_pred is None:
-        y_pred = target_var
+    # if isinstance(y, str):
+    #     target_var = y
+    # else:
+    #     (target_var,) = y.data_vars
+    # independent_var = list(x.data_vars)
 
-    if isinstance(y_pred, xr.Dataset):
-        raise TypeError(
-            "y_pred can't be a dataset because multiple target variables are not supported yet."
-        )
+    # if y_pred is None:
+    #     y_pred = target_var
 
-    if not isinstance(y_pred, xr.DataArray):
-        y_pred = process_group_variables_coords(
-            dt,
-            group=group,
-            var_names=y_pred,
-            filter_vars=filter_vars,
-            coords=coords,
-        )
+    # if isinstance(y_pred, xr.Dataset):
+    #     raise TypeError(
+    #         "y_pred can't be a dataset because multiple target variables are not supported yet."
+    #     )
 
-    if x_pred is None:
-        x_pred = independent_var
+    # if not isinstance(y_pred, xr.DataArray):
+    #     y_pred = process_group_variables_coords(
+    #         dt,
+    #         group="posterior_predictive",
+    #         var_names=y_pred,
+    #         filter_vars=filter_vars,
+    #         coords=coords,
+    #     )
 
-    if not isinstance(x_pred, xr.DataArray | xr.Dataset):
-        if group == "predictions":
-            x_pred = process_group_variables_coords(
-                dt,
-                group="predictions_constant_data",
-                var_names=x_pred,
-                filter_vars=filter_vars,
-                coords=coords,
-            )
-        else:
-            x_pred = x
+    # if x_pred is None:
+    #     x_pred = independent_var
 
-    if isinstance(ci_prob, Sequence):
-        x_with_prob = x.expand_dims(dim={"prob": ci_prob})
+    # if not isinstance(x_pred, xr.DataArray | xr.Dataset):
+    #     if group == "predictions":
+    #         x_pred = process_group_variables_coords(
+    #             dt,
+    #             group="predictions_constant_data",
+    #             var_names=x_pred,
+    #             filter_vars=filter_vars,
+    #             coords=coords,
+    #         )
+    #     else:
+    #         x_pred = x
+
+    x_pred = process_group_variables_coords(
+        dt,
+        group="constant_data",
+        var_names=x,
+        filter_vars=filter_vars,
+        coords=coords,
+    )
+
+    y_pred = process_group_variables_coords(
+        dt,
+        group=group,
+        var_names=y,
+        filter_vars=filter_vars,
+        coords=coords,
+    )
+
+    observed_x = process_group_variables_coords(
+        dt,
+        group="constant_data",
+        var_names=x,
+        filter_vars=filter_vars,
+        coords=coords,
+    )
+
+    observed_y = extract(dt, group="observed_data", combined=False)
+
+    if isinstance(ci_prob, (list | tuple | np.ndarray)):
+        x_with_prob = x_pred.expand_dims(dim={"prob": ci_prob})
     else:
-        x_with_prob = x
+        x_with_prob = x_pred
 
     plot_bknd = import_module(f".backend.{backend}", package="arviz_plots")
     bg_color = plot_bknd.get_background_color()
@@ -243,10 +266,10 @@ def plot_lm(
         pc_kwargs.setdefault("cols", "__variable__")
         pc_kwargs["figure_kwargs"] = pc_kwargs.get("figure_kwargs", {}).copy()
         pc_kwargs["aes"] = pc_kwargs.get("aes", {}).copy()
-        if isinstance(ci_prob, Sequence):
+        if isinstance(ci_prob, (list | tuple | np.ndarray)):
             if "alpha" not in pc_kwargs["aes"]:
                 pc_kwargs["aes"].setdefault("alpha", ["prob"])
-                pc_kwargs["alpha"] = np.linspace(0.1, 0.5, len(ci_prob))
+                pc_kwargs["alpha"] = np.logspace(1, 0.1, len(ci_prob)) / 10
             else:
                 warnings.warn(
                     "When multiple credible intervals are plotted, "
@@ -255,7 +278,7 @@ def plot_lm(
                 )
 
         pc_kwargs["aes"].setdefault("color", ["__variable__"])
-        pc_kwargs = set_wrap_layout(pc_kwargs, plot_bknd, x)
+        pc_kwargs = set_wrap_layout(pc_kwargs, plot_bknd, x_pred)
         plot_collection = PlotCollection.wrap(
             x_with_prob,
             backend=backend,
@@ -269,20 +292,20 @@ def plot_lm(
     aes_by_visuals.setdefault(
         "central_line", plot_collection.aes_set.difference({"alpha", "color"})
     )
-    if isinstance(ci_prob, Sequence):
+    if isinstance(ci_prob, (list | tuple | np.ndarray)):
         aes_by_visuals.setdefault("ci_line", {"alpha"})
         aes_by_visuals.setdefault(
-            "ci_fill", set(aes_by_visuals.get("ci_fill", {})).union({"color", "alpha"})
+            "ci_band", set(aes_by_visuals.get("ci_band", {})).union({"color", "alpha"})
         )
     else:
         aes_by_visuals.setdefault(
-            "ci_fill", set(aes_by_visuals.get("ci_fill", {})).union({"color"})
+            "ci_band", set(aes_by_visuals.get("ci_band", {})).union({"color"})
         )
 
     # calculations for credible interval
     ci_fun = azs.hdi if ci_kind == "hdi" else azs.eti
-    ci_dims, _, fill_ignore = filter_aes(plot_collection, aes_by_visuals, "ci_fill", sample_dims)
-    if isinstance(ci_prob, Sequence):
+    ci_dims, _, fill_ignore = filter_aes(plot_collection, aes_by_visuals, "ci_band", sample_dims)
+    if isinstance(ci_prob, (list | tuple | np.ndarray)):
         ci_data = xr.concat(
             [
                 ci_fun(
@@ -298,18 +321,20 @@ def plot_lm(
     central_line_dims, _, _ = filter_aes(
         plot_collection, aes_by_visuals, "central_line", sample_dims
     )
-    if line_kind == "mean":
-        line_data = y_pred.mean(dim=central_line_dims, **stats.get("point_estimate", {}))
-    elif line_kind == "median":
-        line_data = y_pred.median(dim=central_line_dims, **stats.get("point_estimate", {}))
-    elif line_kind == "mode":
-        line_data = azs.mode(y_pred, dim=central_line_dims, **stats.get("point_estimate", {}))
+    if point_estimate == "mean":
+        pe_value = y_pred.mean(dim=central_line_dims, **stats.get("point_estimate", {}))
+    elif point_estimate == "median":
+        pe_value = y_pred.median(dim=central_line_dims, **stats.get("point_estimate", {}))
+    else:
+        pe_value = azs.mode(y_pred, dim=central_line_dims, **stats.get("point_estimate", {}))
+
+    idx = x_pred[x].argsort()
+    x_pred = x_pred.isel(dim_0=idx.values)
+    pe_value = pe_value[y].isel({f"{y}_dim_0": idx.values})
+    ci_lower = ci_data.sel(ci_bound="lower").isel({f"{y}_dim_0": idx.values})[y]
+    ci_upper = ci_data.sel(ci_bound="upper").isel({f"{y}_dim_0": idx.values})[y]
 
     lines = plot_bknd.get_default_aes("linestyle", 2, {})
-
-    ci_lower = ci_data.sel(ci_bound="lower")
-    ci_upper = ci_data.sel(ci_bound="upper")
-
     # upper and lower lines of credible interval
     ci_line_kwargs = copy(visuals.get("ci_line", False))
     if ci_line_kwargs is not False:
@@ -326,7 +351,7 @@ def plot_lm(
             line_xy,
             "ci_line",
             x=x_pred,
-            y=ci_lower[target_var],
+            y=ci_lower,
             ignore_aes=ci_line_ignore,
             **ci_line_kwargs,
         )
@@ -335,62 +360,62 @@ def plot_lm(
             line_xy,
             "ci_line",
             x=x_pred,
-            y=ci_upper[target_var],
+            y=ci_upper,
             ignore_aes=ci_line_ignore,
             **ci_line_kwargs,
         )
 
     # fill between lines of credible interval
-    fill_kwargs = copy(visuals.get("ci_fill", {}))
+    fill_kwargs = copy(visuals.get("ci_band", {}))
     if fill_kwargs is not False:
         plot_collection.map(
             fill_between_y,
-            "ci_fill",
+            "ci_band",
             x=x_pred,
-            y_bottom=ci_lower[target_var],
-            y_top=ci_upper[target_var],
+            y_bottom=ci_lower,  # [target_var],
+            y_top=ci_upper,  # [target_var],
             ignore_aes=fill_ignore,
             **fill_kwargs,
         )
 
-    # mean/median/mode line
-    central_line_kwargs = copy(visuals.get("central_line", {}))
-    if central_line_kwargs is not False:
-        _, central_line_aes, central_line_ignore = filter_aes(
-            plot_collection, aes_by_visuals, "central_line", sample_dims
+    # point estimate line
+    pe_line_kwargs = copy(visuals.get("pe_line", {}))
+    if pe_line_kwargs is not False:
+        _, pe_line_aes, pe_line_ignore = filter_aes(
+            plot_collection, aes_by_visuals, "pe_line", sample_dims
         )
-        if "color" not in central_line_aes:
-            central_line_kwargs.setdefault("color", contrast_color)
-        if "alpha" not in central_line_aes:
-            central_line_kwargs.setdefault("alpha", 0.6)
+        if "color" not in pe_line_aes:
+            pe_line_kwargs.setdefault("color", contrast_color)
+        if "alpha" not in pe_line_aes:
+            pe_line_kwargs.setdefault("alpha", 0.6)
         plot_collection.map(
             line_xy,
-            "central_line",
+            "pe_line",
             x=x_pred,
-            y=line_data[target_var],
-            ignore_aes=central_line_ignore,
-            **central_line_kwargs,
+            y=pe_value,  # [target_var],
+            ignore_aes=pe_line_ignore,
+            **pe_line_kwargs,
         )
 
     # scatter plot
-    original_scatter_kwargs = copy(visuals.get("scatter", {}))
-    if original_scatter_kwargs is not False:
+    observed_scatter_kwargs = copy(visuals.get("observed_scatter", {}))
+    if observed_scatter_kwargs is not False:
         _, scatter_aes, scatter_ignore = filter_aes(
-            plot_collection, aes_by_visuals, "scatter", sample_dims
+            plot_collection, aes_by_visuals, "observed_scatter", sample_dims
         )
         if "alpha" not in scatter_aes:
-            original_scatter_kwargs.setdefault("alpha", 0.3)
+            observed_scatter_kwargs.setdefault("alpha", 0.3)
         if "color" not in scatter_aes:
-            original_scatter_kwargs.setdefault("color", contrast_gray_color)
+            observed_scatter_kwargs.setdefault("color", contrast_gray_color)
         if "width" not in scatter_aes:
-            original_scatter_kwargs.setdefault("width", 0)
+            observed_scatter_kwargs.setdefault("width", 0)
         plot_collection.map(
             scatter_xy,
-            "scatter",
-            x=x,
-            y=y[target_var],
+            "observed_scatter",
+            x=observed_x,
+            y=observed_y,
             ignore_aes=scatter_ignore,
-            **original_scatter_kwargs,
+            **observed_scatter_kwargs,
         )
 
     # x-axis label
@@ -400,7 +425,7 @@ def plot_lm(
         plot_collection.map(
             labelled_x,
             "xlabel",
-            data=x,
+            data=x_pred,
             labeller=labeller,
             subset_info=True,
             ignore_aes=xlabel_ignore,
@@ -414,7 +439,7 @@ def plot_lm(
         plot_collection.map(
             labelled_y,
             "ylabel",
-            text=target_var,
+            text=y,
             ignore_aes=ylabel_ignore,
             **ylabel_kwargs,
         )
