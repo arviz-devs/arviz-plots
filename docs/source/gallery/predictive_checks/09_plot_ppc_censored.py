c4a275ee7f09f55b58654a5c00d70935d3b2b369
aloctavodia <aloctavodia@gmail.com>
2025-09-23 15:35:20 +0300

    add gallery example

---
+++ docs/source/gallery/predictive_checks/09_plot_ppc_censored.py
@@ -0,0 +1,23 @@
+"""
+# Censored posterior predictive checks
+Plot Kaplan-Meier survival curves for posterior predictive checking of censored data.
+---
+:::{seealso}
+API Documentation: {func}`~arviz_plots.plot_ppc_censored`
+:::
+"""
+
+from arviz_base import load_arviz_data
+
+import arviz_plots as azp
+
+azp.style.use("arviz-variat")
+
+data = load_arviz_data("censored_cats")
+
+
+pc = azp.plot_ppc_censored(
+    backend="none",
+)
+
+pc.show()
