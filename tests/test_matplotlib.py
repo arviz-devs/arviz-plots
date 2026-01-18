b93916a37f882c27d21b3d955ad009f1cd43640d
Oriol (VANT Edge) <oriol.abril.pla@gmail.com>
2026-01-18 21:28:38 +0100

    fix backend tests skipping

--- tests/test_matplotlib.py
+++ tests/test_matplotlib.py
@@ -1,19 +1,16 @@
 # pylint: disable=no-self-use, redefined-outer-name, wrong-import-position
 """Tests specific to the matplotlib backend."""
-import pytest
-
-if pytest.config.get_config().getoption("--skip-mpl"):
-    pytest.skip(
-        reason="Requested skipping matplolib tests via command line argument",
-        allow_module_level=True,
-    )
-
 import matplotlib.pyplot as plt
+import pytest
 from matplotlib.lines import Line2D
 
 from arviz_plots.backend.matplotlib import line
 
-pytestmark = [pytest.mark.usefixtures("clean_plots")]
+pytestmark = [
+    pytest.mark.usefixtures("clean_plots"),
+    pytest.mark.usefixtures("check_skips"),
+    pytest.mark.matplotlib,
+]
 
 
 @pytest.fixture(scope="function")
@@ -32,6 +29,6 @@ def test_line(fig_ax):
 
 def test_line_args(fig_ax):
     ax = fig_ax[1]
-    line_obj = line([0, 1, 2], [0, 2, 1], ax, color="jade", width=2.2)
+    line_obj = line([0, 1, 2], [0, 2, 1], ax, color="orange", width=2.2)
     assert line_obj.get_linewidth() == 2.2
-    assert line_obj.get_color() == "jade"
+    assert line_obj.get_color() == "orange"
