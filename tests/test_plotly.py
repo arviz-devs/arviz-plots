40c8d52f0df0e2ef35bf972adc09c13c9cce2b24
Oriol (VANT Edge) <oriol.abril.pla@gmail.com>
2026-01-25 19:09:13 +0100

    allow running tests with missing dependencies

--- tests/test_plotly.py
+++ tests/test_plotly.py
@@ -1,7 +1,15 @@
 # pylint: disable=no-self-use, redefined-outer-name, wrong-import-position
 """Tests specific to the plotly backend."""
-import plotly.graph_objects as go
+import os
+
 import pytest
+
+if os.environ.get("ARVIZ_REQUIRE_ALL_DEPS", False):
+    import plotly  # pylint: disable=unused-import
+else:
+    pytest.importorskip("plotly")
+
+import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 from arviz_plots.backend.plotly import line
