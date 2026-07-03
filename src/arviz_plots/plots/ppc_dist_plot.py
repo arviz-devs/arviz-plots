24f046fd5d9f73731fa4a3f475d65979c0a266ab
aloctavodia <aloctavodia@gmail.com>
2026-07-03 16:08:33 +0300

    fix collision

--- src/arviz_plots/plots/ppc_dist_plot.py
+++ src/arviz_plots/plots/ppc_dist_plot.py
@@ -178,7 +178,7 @@ def plot_ppc_dist(
         backend=backend,
         stats=stats,
         warn_discrete_dist=True,
-        warn_if_prior_predictive=False,
+        warn_prior_predictive=False,
     )
 
     if plot_collection is None:
