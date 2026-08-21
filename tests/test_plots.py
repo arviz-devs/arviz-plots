1d7d075a017871cb30a75518c8e37d6946ca810f
Kush Pathak <b25cs1051@iitj.ac.in>
2026-08-21 16:32:02 +0530

    test(plots): add test for Dataset y_obs input to cover missing lines

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -546,6 +546,12 @@ class TestPlots:  # pylint: disable=too-many-public-methods
         assert "figure" in pc.viz.data_vars
         assert "observed_scatter" in pc.viz.children
 
+    def test_plot_lm_y_obs_dataset(self, datatree_regression, backend):
+        y_obs_ds = datatree_regression["observed_data"].ds
+        pc = plot_lm(datatree_regression, y="y", y_obs=y_obs_ds, backend=backend)
+        assert "figure" in pc.viz.data_vars
+        assert "observed_scatter" in pc.viz.children
+
     @pytest.mark.parametrize("coverage", (True, False))
     def test_plot_loo_pit(self, datatree, coverage, backend):
         pc = plot_loo_pit(datatree, coverage=coverage, backend=backend)
